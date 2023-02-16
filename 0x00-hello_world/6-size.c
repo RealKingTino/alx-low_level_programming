fa8dded850ed572c22508a5439ae43b63f10492a
RealKingTino <tmowizzy@gmail.com>
2023-02-16 13:21:11 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- * main - This program uses the printf function to print the size of various types
- * Return: 0 (success)
+ * main - This program uses the printf to print the size of various types
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("Size of char is %lu bytes\n", sizeof(char));
-	printf("Size of int is %lu byte(s)\n", sizeof(int));
-	printf("Size of long int is %lu byte(s)\n", sizeof(long int));
-	printf("Size of float is %lu byte(s)\n", sizeof(float));
-	printf("Size of long long int is %lu byte(s)\n", sizeof(long long int));
-	return (0);
+printf("This is a piece of art\n");
+return (0);
 }

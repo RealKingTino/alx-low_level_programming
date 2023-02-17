d9319db9a80471f00069e2c482b7cffc7994f2a6
RealKingTino <tmowizzy@gmail.com>
2023-02-16 16:33:36 -0800

    101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistdio.h>
 /**
- * main - A program the prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
  * Return: 1
  */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(1);
+return (1);
 }

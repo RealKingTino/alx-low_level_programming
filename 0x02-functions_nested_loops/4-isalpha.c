c94f9413672a82b887d22df091294dbb66de92a0
RealKingTino <tmowizzy@gmail.com>
2023-02-21 18:07:31 -0800

    A function that checks for alphabetic char

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,9 @@
 /**
  * _islapha - checks for alphabetic character
  * @c: is the character to be checked
- * Return: 1 if c is a leetter, 0 otherwise
+ * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int c)
+int _islapha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }

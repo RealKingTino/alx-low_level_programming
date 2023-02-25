cd6c01652dc158a1671969b0714b6c72fc3ed28c
RealKingTino <tmowizzy@gmail.com>
2023-02-25 05:51:10 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,6 +5,8 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
+ * For numbers which are multiples of both 3 and 5 print FizzBuzz
+ * Each number or word should be seperated by space
  * Return: Always 0 (Success)
  */
 int main(void)

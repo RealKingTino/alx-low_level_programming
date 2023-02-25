aeccad0f94d5009489caf409287228757c1154c1
RealKingTino <tmowizzy@gmail.com>
2023-02-25 12:53:48 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,25 +17,21 @@ int main(void)
 	{
 		if (((i % 3) == 0) && ((i % 5) != 0))
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (((i % %) == 0) && ((i % 3) != 0))
+		else if (((i % 5) == 0) && ((i % 3) != 0))
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else if (((i % 5) == 0 && ((i % 3) ==0))
+		else if (((i % 5) == 0) && ((i % 3) == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
 			printf("%d", i);
 		}
-		if (!(i == 100))
-		{
-			printf(" ");
-		}
 	}
 	printf("\n");
-	retunt (0);
+	return (0);
 }

6627397109bb498fac2a4d4ebc7e492a06851b0f
RealKingTino <tmowizzy@gmail.com>
2023-03-01 12:32:07 -0800

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,16 @@
  * @src: input value
  *
  * Return: void
- */r
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	i = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];

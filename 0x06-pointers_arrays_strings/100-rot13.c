31d70f0436a2ddd1a1ece5a5cf6fa9cbd416650d
RealKingTino <tmowizzy@gmail.com>
2023-03-02 13:09:26 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - a function that encodes a string using rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+char *rot13(char *str) {
+	char *p = str;
+	int i;
+
+while (*p) 
+{
+	if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'm')) {
+		*p += 13;
+	}
+	else if ((*p >= 'N' && *p <= 'Z') || (*p >= 'n' && *p <= 'z')) 
+	{
+		*p -= 13;
+	}
+	p++;
+}
+return str;
+}
+
+int main() 
+{
+	char str[] = "Hello, world!";
+	printf("Original: %s\n", str);
+	rot13(str);
+	printf("Encoded: %s\n", str);
+	return 0;
+}

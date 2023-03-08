84fb7502dfb8502ab4f7df1a5f46f25990e12814
RealKingTino <tmowizzy@gmail.com>
2023-03-08 12:51:12 -0800

    eturns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,16 +8,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-
-	if (n == 1)
-		return (1);
-
 	if (n < 0)
 		return (-1);
-
-	return (_sqrt_recursion(n, 1));
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
@@ -27,13 +20,11 @@ int _sqrt_recursion(int n)
  *
  * Return: natural square root of n, -1 if it doesn't have one
  */
-int _sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int n, int i)
 {
-	if (n == i * i)
-		return (i);
-
-	if (n < i * i)
+	if (i * i > n)
 		return (-1);
-
-	return (_sqrt_recursion(n, i + 1));
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }

1bffdff6f4581c1ca47c84525b439eb32bf8c096
RealKingTino <tmowizzy@gmail.com>
2023-03-12 04:30:27 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,73 @@
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
- * add_positive_numbers - adds positive numbers
- * @n1: First number
- * @n2: Second number
- * @n3: Third number
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: Sum of the three numbers
+ * Return: the int converted from the string
  */
-
-int add_positive_numbers(int n1, int n2, int n3)
+int _atoi(char *s)
 {
-	int sum;
-
-	if (n1 > 0 && n2 > 0 && n3 > 0)
+	int i, d, n, len, f, digit;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		sum = n1 + n2 + n3;
-		printf("%d\n", sum);
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	else
+	if (f == 0)
+		return (0);
+	return (n);
+}
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+	sum = 0;
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
 	}
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
 	return (0);
 }

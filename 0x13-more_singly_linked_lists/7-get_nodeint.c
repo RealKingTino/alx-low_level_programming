538e15349dc92754af07b2956924392664b22441
RealKingTino <tmowizzy@gmail.com>
2023-04-03 13:03:19 -0700

    fixed a bug in 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (current != NULL)
 	{
 		if (i == index)
-			return (cuurent);
+			return (curent);
 		i++;
 		current = current->next;
 	}

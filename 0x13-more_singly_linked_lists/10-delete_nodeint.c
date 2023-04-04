52c186b8f8243dbc7090d47d71012f3ed925f836
RealKingTino <tmowizzy@gmail.com>
2023-04-04 02:35:08 -0700

    fixed a bug in 8-sum_listint.c & completed 10-delete_nodeint.c task

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the node at index of a linked list
+ * @head: pointer to pointer to head node
+ * @index: index of node to delete
+ *
+ * Return: 1 if successful, -1 otherwise
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *temp;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	current = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+
+	temp = current->next;
+	if (temp == NULL)
+		return (-1);
+
+	current->next = temp->next;
+	free(temp);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * sum_listint - returns the sum of all data (n) of a linked list
- * @@head: Pointer to the first node of the linked list
+ * @head: Pointer to the first node of the linked list
  *
  * Return: The sum of all data of the linked list
  */

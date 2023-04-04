cf4941dcf65e61a2fd9c30e309bed97a5593b473
RealKingTino <tmowizzy@gmail.com>
2023-04-04 02:04:02 -0700

    completed 8-sum_listint.c & 9-insert_nodeint.c task

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all data (n) of a linked list
+ * @@head: Pointer to the first node of the linked list
+ *
+ * Return: The sum of all data of the linked list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}

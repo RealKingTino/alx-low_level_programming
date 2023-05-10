7812a93bf85b6930194c6dabab426f20dc76c88d
RealKingTino <tmowizzy@gmail.com>
2023-05-09 23:07:45 -0700

    update on header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,14 @@
 #include <stdio.h>
 #define BUFSIZE 1024
 #include <elf.h>
+#include <stdio.h>
 
+#define ERROR 98
+
+int validate_arguments(int argc, char **argv);
+int open_file(char *filename);
+int read_header(int fd, Elf64_Ehdr *header);
+void print_header_info(Elf64_Ehdr header);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
